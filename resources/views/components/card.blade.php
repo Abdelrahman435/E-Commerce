aa2cf7513f8cd991374f11b0a779e9d563a7b633
Abdelrahman435 <ah0383723@gmail.com>
2025-11-26 21:44:41 +0200

    Add .gitignore files for storage directories to exclude unnecessary files
    - Created .gitignore in storage/app to ignore all files except public/ and itself.
    - Created .gitignore in storage/app/public to ignore all files except itself.
    - Created .gitignore in storage/framework to ignore various framework-related files.
    - Created .gitignore in storage/framework/cache to ignore all files except data/ and itself.
    - Created .gitignore in storage/framework/cache/data to ignore all files except itself.
    - Created .gitignore in storage/framework/sessions to ignore all files except itself.
    - Created .gitignore in storage/framework/testing to ignore all files except itself.
    - Created .gitignore in storage/framework/views to ignore all files except itself.
    - Created .gitignore in storage/logs to ignore all files except itself.

--- resources/views/components/card.blade.php
+++
@@ -1,3 +0,0 @@
-        <div {{ $attributes->class(['bg-white rounded-lg shadow-md p-6'])}}>
-            {{ $slot }}
-        </div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
-use App\Http\Controllers\ProductController;
+
 use Illuminate\Support\Facades\Route;
 
-Route::get('', fn()=> to_route('products.index'));
-Route::resource('products', ProductController::class)->only(['index', 'show']);
+Route::get('/', function () {
+    return view('welcome');
+});
