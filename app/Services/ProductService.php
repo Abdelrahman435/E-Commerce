ca16c73b893e37f584f3fb685e04851e1698712e
Abdelrahman435 <ah0383723@gmail.com>
2025-11-27 01:28:25 +0200

    fix: Update caching mechanism in ProductService and correct request parameter usage

--- app/Services/ProductService.php
+++ app/Services/ProductService.php
@@ -11,9 +11,9 @@ class ProductService
     {
         $cacheKey = 'products:' . md5(json_encode($request->query()));
 
-        return Cache::remember($cacheKey, now()->addMinutes(10), function () use ($request) {
+        return Cache::tags(['products'])->remember($cacheKey, now()->addMinutes(10), function () use ($request) {
             return Product::query()
-                ->when($request->name, fn($q) => $q->where('title', 'like', "%{$request->name}%"))
+                ->when($request->title, fn($q) => $q->where('title', 'like', "%{$request->title}%"))
                 ->when($request->min_price, fn($q) => $q->where('price', '>=', $request->min_price))
                 ->when($request->max_price, fn($q) => $q->where('price', '<=', $request->max_price))
                 ->with('images')
@@ -23,17 +23,20 @@ class ProductService
 
     public function create(array $data)
     {
+        Cache::tags(['products'])->flush();
         return auth()->user()->products()->create($data);
     }
 
     public function update(Product $product, array $data)
     {
+        Cache::tags(['products'])->flush();
         $product->update($data);
         return $product;
     }
 
-    public function delete(Product $product)    
+    public function delete(Product $product)
     {
+        Cache::tags(['products'])->flush();
         $product->delete();
         return true;
     }
